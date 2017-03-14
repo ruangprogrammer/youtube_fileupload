a556014f57bb540618735c4409c720115535935a
Swapnil Singh <swappp7singh@gmail.com>
2017-03-14 10:45:57 +0530

    Done

---
+++ routes/profile.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
+
+var upload = multer({ storage: storage }).single('profileImage');
+
+
+router.post('/', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            // An error occurred when uploading
+        }
+        res.json({
+            success: true,
+            message: 'Image uploaded!'
+        });
+
+        // Everything went fine
+    })
+});
+
+
+module.exports = router;
